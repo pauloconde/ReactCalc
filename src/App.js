12179672507ae104758c60598dd83dc05028d3db
Paulo Conde <pauloconde@gmail.com>
2024-03-01 21:54:09 -0400

    Initial Conmit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Boton from './components/Boton'
+import BotonClear from './components/BotonClear'
+import Pantalla from './components/Pantalla'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='calculator'>
+        <Pantalla input='12345.06'/>
+        <div className='line'>
+          <Boton>1</Boton>
+          <Boton>2</Boton>
+          <Boton>3</Boton>
+          <Boton>+</Boton>
+        </div>
+        <div className='line'>
+          <Boton>4</Boton>
+          <Boton>5</Boton>
+          <Boton>6</Boton>
+          <Boton>-</Boton>
+        </div>
+        <div className='line'>
+          <Boton>7</Boton>
+          <Boton>8</Boton>
+          <Boton>9</Boton>
+          <Boton>*</Boton>
+        </div>
+        <div className='line'>
+          <Boton ></Boton>
+          <Boton >0</Boton>
+          <Boton >.</Boton>
+          <Boton >/</Boton>
+        </div>
+        <div className='line'>
+          <BotonClear >
+            Clear
+          </BotonClear>
+        </div>
+    </div>
     </div>
   );
 }
---
+++ src/components/Boton.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/Boton.css'
+
+export default function Button(props) {
+    const isOperator = value => {
+        return isNaN(value) && (value !== '.') && (value !== '=');
+      };
+    
+      return (
+        <div
+          className={`button-container ${isOperator(props.children) ? 'operator' : ''}`.trimEnd()}
+          onClick={() => props.handleClic(props.children)}>
+          {props.children}
+        </div>
+      );
+}
